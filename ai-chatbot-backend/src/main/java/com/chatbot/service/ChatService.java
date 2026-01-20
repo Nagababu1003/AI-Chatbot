1f101928f4c54c549c2034a970b5b3b7fab32823
Nagababu Bejawada <80879408+Nagababu1003@users.noreply.github.com>
2026-01-20 08:25:50 +0530

    Update ChatService.java

--- ai-chatbot-backend/src/main/java/com/chatbot/service/ChatService.java
+++ ai-chatbot-backend/src/main/java/com/chatbot/service/ChatService.java
@@ -1,44 +1,5 @@
 package com.chatbot.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.stereotype.Service;
-
-import com.chatbot.dto.ChatRequest;
-import com.chatbot.dto.ChatResponse;
-import com.chatbot.entity.ChatMessage;
-import com.chatbot.repository.ChatMessageRepository;
-
-
-@Service
-public class ChatService {
-
-	private final ChatClient chatClient;
-	private final ChatMessageRepository repository;
-	
-	public ChatService(ChatClient.Builder builder, ChatMessageRepository repository) {
-		this.chatClient = builder.build();
-		this.repository = repository;
-	}
-	
-	public ChatResponse chat(ChatRequest request) {
-		String aiReply=  chatClient.prompt().user(request.getMessage())
-				.call().content();
-		
-		ChatMessage chatmessage=ChatMessage.builder()
-				.userMessage(request.getMessage())
-				.aiResponse(aiReply)
-				.createdAt(LocalDateTime.now())
-				.build();
-		
-		repository.save(chatmessage);
-		
-		return new ChatResponse(aiReply);
-	}
-	
-	public List<ChatMessage> getHistory() {
-		return repository.findAll();
-	}
+public interface ChatService {
+    String chat(String message);
 }
